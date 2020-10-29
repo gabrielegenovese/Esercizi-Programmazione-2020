82eef7471ef2db50e68ff82ae7a530050be4ddf2
Gabriele <gabrigeno@gmail.com>
2020-10-29 13:50:02 +0100

    commit

--- Esercizi/Esercizi_stringhe/Parametri/main.cpp
+++ Esercizi/Esercizi_stringhe/Parametri/main.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 void parola(char str[], char c, char dest[])
 {
-    int j = 0,i = 1;
+    int j = 0, i = 1;
+
+    // trovo la parola (per trovare la prima parola bisogna mettere prima uno spazio)
     while (str[i] != '\0' && (str[i] != c || str[i-1] != ' ')) i++;
     
+    // la inserisco in dest
     while (str[i] != '\0' && str[i] != ' ')
     {
         dest[j] = str[i];
@@ -33,7 +36,7 @@ int main()
 {
     int length = 30;
 
-    char str[] = "ciao mi chiamo gabriele e sono un informatico\0";
+    char str[] = " ciao mi chiamo gabriele e sono un informatico\0";
     char dest[length], find;
 
     cout << str << endl;
